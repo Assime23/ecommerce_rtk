7e05a5ba52b9562a18a4c31e9af40ef1863e79ef
assime23 <assimekere@gmail.com>
2025-01-31 17:41:27 -0500

    V2.2

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,6 +1,50 @@
-
+import { createSlice } from '@reduxjs/toolkit';
 const CartSlice = ({
+      name: 'cart',
+  initialState,
+  reducers: {
+    addItemToCart(state, action) {
+      const existingItem = state.cartItems.find(item => item.id === action.payload.id);
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.cartItems.push({ ...action.payload, quantity: 1 });
+      }
+    },
+
+    removeItemFromCart(state, action) {
+      state.cartItems = state.cartItems.filter(item => item.id !== action.payload);
+    },
+
+    clearCart(state) {
+      state.cartItems = [];
+    },
+
+    clearCart(state) {
+      state.cartItems = [];
+    },
+
+    decreaseItemQuantity(state, action) {
+      const itemToDecrease = state.cartItems.find(item => item.id === action.payload);
+      if (itemToDecrease && itemToDecrease.quantity > 1) {
+        itemToDecrease.quantity -= 1;
+      }
+    },
+
+  }
  
 });
 
+export const {
+  addItemToCart,
+  removeItemFromCart,
+  clearCart,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+} = CartSlice.actions;
+export default CartSlice.reducer;
+
+const initialState =createSlice( {
+  cartItems: [],
+});
 
